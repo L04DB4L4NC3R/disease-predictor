3f562682c19c35e345ef211e4995c40a41478b59
angad <angadsharma1016@gmail.com>
2018-07-30 18:45:47 +0530

    dotenv config

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
+import os
 from flask import Flask,render_template,jsonify
 
 app = Flask(__name__)
-
-
 
 
 @app.route("/",methods=["GET"])
 def index():
     return jsonify({"status":True})
 
+
+@app.route("/weather/<place>",methods=["GET"])
+def getWeather(place):
+    return jsonify({"data":place})
 
 
 if __name__ == '__main__':
